e0da32a74c62b57cd37ede55bfd88467662be19c
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-01-10 00:23:11 -0500

    Inlcude ordering
    Attempting to fix through the web interface as i don't have rust installed right now...

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,17 +1,18 @@
 extern crate rust_guessing_game;
 use rust_guessing_game::my_funcs;
+use std::cmp::Ordering;
 
 #[test]
 fn compare_greater_than() {
-    assert!(my_funcs::compare(1,0) == Greater);
+    assert!(my_funcs::compare(1,0) == Ordering::Greater);
 }
 
 #[test]
 fn compare_less_than() {
-    assert!(my_funcs::compare(0,1) == Less);
+    assert!(my_funcs::compare(0,1) == Ordering::Less);
 }
 
 #[test]
 fn compare_equal_to() {
-    assert!(my_funcs::compare(1,1) == Equal);
+    assert!(my_funcs::compare(1,1) == Ordering::Equal);
 }
