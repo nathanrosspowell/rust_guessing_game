3707ce032358fc559d9e3c99dfeb35239307f461
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-02-09 10:31:38 -0500

    uint -> usize

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,18 @@ use rust_guessing_game::my_funcs;
 #[cfg(not(test))]
 fn main() {
     println!("Guess the magic number!");
-    let magic_number = ( rand::random::<uint>() % 100u ) + 1u;
+    let magic_number = ( rand::random::<usize>() % 100u ) + 1u;
     loop {
         let input = io::stdin().read_line().ok().expect("That was bad input");
-        let input_optoin_uint: Option<uint> = input.parse();
-        let input_uint = match input_optoin_uint {
+        let input_optoin_usize: Option<usize> = input.parse();
+        let input_usize = match input_optoin_usize {
             Some(num) => num,
             None      => {
                 println!("Please input a number");
                 continue;
             }
         };
-        match my_funcs::compare(input_uint, magic_number) {
+        match my_funcs::compare(input_usize, magic_number) {
             Ordering::Less    => println!("Guess higher."),
             Ordering::Greater => println!("Guess lower."),
             Ordering::Equal   => { 
