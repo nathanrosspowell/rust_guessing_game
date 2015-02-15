b69515fc730bfb7b5fac82b6363afa7277464301
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-02-15 17:18:30 -0500

    Fix the guessing game....
    I actually cloned the repro and made the needed updates without guessing
    this time.

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,18 @@ use rust_guessing_game::my_funcs;
 #[cfg(not(test))]
 fn main() {
     println!("Guess the magic number!");
-    let magic_number = ( rand::random::<usize>() % 100 ) + 1;
+    let magic_number = ( rand::random::<u32>() % 100 ) + 1;
     loop {
         let input = io::stdin().read_line().ok().expect("That was bad input");
-        let input_optoin_usize: Option<usize> = input.parse();
-        let input_usize = match input_optoin_usize {
-            Some(num) => num,
-            None      => {
-                println!("Please input a number");
+        let input_num: Result<u32, _> = input.trim().parse();
+        let num = match input_num {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a number!");
                 continue;
             }
         };
-        match my_funcs::compare(input_usize, magic_number) {
+        match my_funcs::compare(num, magic_number) {
             Ordering::Less    => println!("Guess higher."),
             Ordering::Greater => println!("Guess lower."),
             Ordering::Equal   => { 
--- src/my_funcs.rs
+++ src/my_funcs.rs
@@ -1,6 +1,6 @@
 use std::cmp::Ordering;
 
-pub fn compare(input: usize, test: usize) -> Ordering {
+pub fn compare(input: u32, test: u32) -> Ordering {
     if input < test {
         Ordering::Less
     }
