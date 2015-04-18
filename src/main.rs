d6d57d29d341f59b3d1c540bbcb118f85e1d8a68
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-04-18 16:32:11 -0400

    fixes to io and rand

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate rust_guessing_game;
+extern crate rand;
 
-use std::old_io as io;
-use std::rand;
+use std::io as io;
 use std::cmp::Ordering;
 
 use rust_guessing_game::my_funcs;
@@ -11,7 +11,8 @@ fn main() {
     println!("Guess the magic number!");
     let magic_number = ( rand::random::<u32>() % 100 ) + 1;
     loop {
-        let input = io::stdin().read_line().ok().expect("That was bad input");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).ok().expect("That was bad input");
         let input_num: Result<u32, _> = input.trim().parse();
         let num = match input_num {
             Ok(num) => num,
